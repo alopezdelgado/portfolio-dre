170836667601a7cd16058c62dc0ccc4fb1ed2e1c
alopezdelgado <andrealdelgado_99@outlook.com>
2023-10-13 13:39:24 -0700

    added styling to nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,38 +6,18 @@ const NavBar = () => {
     return (
         <div className="navBar">
             <div className='myNameBox'>
-                <h1 className='myName'>Dre Lopez Delgado
-                </h1>
+                <h1 className='myName'>Dre Lopez Delgado</h1>
             </div>
             <div className='navLinksBox'>
-                <div className='about-me'>
-                <div className='contact-me'>
-                    <li><NavLink to="/Home">Home
-                    </NavLink>
-                    </li>
-                </div>
-                    <li><NavLink to="/AboutMe">AboutMe
-                    </NavLink>
-                    </li>
-                </div>
-                <div className='experience'>
-                    <li><NavLink to="/Experience">Experience and Education
-                    </NavLink>
-                    </li>
-                </div>
-                <div className='projects'>
-                    <li><NavLink to="/Projects">Projects
-                    </NavLink>
-                    </li>
-                </div>
-                <div className='contact-me'>
-                    <li><NavLink to="/ContactMe">Contact Me
-                    </NavLink>
-                    </li>
-                </div>
+                <li><NavLink to="/Home" activeClassName="activeLink">Home</NavLink></li>
+                <li><NavLink to="/AboutMe" activeClassName="activeLink">About Me</NavLink></li>
+                <li><NavLink to="/Experience" activeClassName="activeLink">Experience and Education</NavLink></li>
+                <li><NavLink to="/Projects" activeClassName="activeLink">Projects</NavLink></li>
+                <li><NavLink to="/ContactMe" activeClassName="activeLink">Contact Me</NavLink></li>
             </div>
         </div>
     )
+
 }
 
 export default NavBar 
